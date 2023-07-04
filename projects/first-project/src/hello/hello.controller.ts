39076198a33ccdc672274e50bae4fe6540baec4f
Tuag <igortuag@gmail.com>
2023-07-04 14:00:05 -0300

    add config to endpoint

--- projects/first-project/src/hello/hello.controller.ts
+++ projects/first-project/src/hello/hello.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { HelloService } from './hello.service';
 
 @Controller('hello')
-export class HelloController {}
+export class HelloController {
+  constructor(private readonly helloService: HelloService) { }
+  @Get()
+  getHello(): string {
+    return this.helloService.getGreeting();
+  }
+}
