608cf5db2e94fc3627340058be92b90c9beadbb9
Tuag <igortuag@gmail.com>
2023-08-23 09:29:35 -0300

    add prisma to constructor

--- projects/second-project/src/app.controller.ts
+++ projects/second-project/src/app.controller.ts
@@ -1,7 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
+import { PrismaService } from './database/prisma.service';
 
 @Controller()
 export class AppController {
+  constructor(
+    private prisma: PrismaService
+  ) {
+
+  }
 
   @Get('hello')
   getHello(): object {
