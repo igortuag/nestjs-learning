7581cb051d0ad636a8a0a6d38aa4ffb6d5d463c9
Tuag <igortuag@gmail.com>
2023-09-02 08:22:46 -0300

    adjust import

--- projects/second-project/src/repositories/prisma/prisma-team-member-repository.ts
+++ projects/second-project/src/repositories/prisma/prisma-team-member-repository.ts
@@ -1,6 +1,6 @@
 import { PrismaService } from "src/database/prisma.service";
 import { TeamMemberRepository } from "../team-member-repository";
-import { randomUUID } from "crypto";
+import { randomUUID } from "node:crypto";
 
 export class PrismaTeamMemberRepository implements TeamMemberRepository {
   constructor(private prisma: PrismaService) { }
