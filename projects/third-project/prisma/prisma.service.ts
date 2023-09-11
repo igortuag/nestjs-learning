a0d05aaaa054dd69bfed0489f38b367e98934160
Tuag <igortuag@gmail.com>
2023-09-11 09:05:18 -0300

    implement modules

--- projects/third-project/prisma/prisma.service.ts
+++ projects/third-project/prisma/prisma.service.ts
@@ -1,13 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient {
+export class PrismaService extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy {
   constructor() {
     super(
       {
         log: ['info', 'warn'],
       }
     );
+  }
+
+  onModuleInit() {
+    throw new Error('Method not implemented.');
+  }
+
+  onModuleDestroy() {
+    throw new Error('Method not implemented.');
   }
 }
