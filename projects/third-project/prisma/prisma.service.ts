9741f5294b602ee5c33ceb3e7c72d513977de227
Tuag <igortuag@gmail.com>
2023-09-11 09:03:31 -0300

    add logs

--- projects/third-project/prisma/prisma.service.ts
+++ projects/third-project/prisma/prisma.service.ts
@@ -4,6 +4,10 @@ import { PrismaClient } from '@prisma/client';
 @Injectable()
 export class PrismaService extends PrismaClient {
   constructor() {
-    super();
+    super(
+      {
+        log: ['info', 'warn'],
+      }
+    );
   }
 }
