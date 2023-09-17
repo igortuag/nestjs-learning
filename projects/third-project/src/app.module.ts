d35796106a7e6916f15770f3ebfa320d7f5da2d3
Tuag <igortuag@gmail.com>
2023-09-17 09:16:38 -0300

    add config module

--- projects/third-project/src/app.module.ts
+++ projects/third-project/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { CreateAccountController } from 'controllers/create-account.controller';
 import { PrismaService } from 'prisma/prisma.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [CreateAccountController],
   providers: [PrismaService],
 })
