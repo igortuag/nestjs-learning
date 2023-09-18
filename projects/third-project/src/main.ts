428a64a9ba21bc5d26fbf341581f9f5edab90c7b
Tuag <igortuag@gmail.com>
2023-09-18 10:14:44 -0300

    add type to variables

--- projects/third-project/src/main.ts
+++ projects/third-project/src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { Env } from 'prisma/env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const configService = app.get(ConfigService);
+  const configService = app.get<ConfigService<Env>>(ConfigService);
   const port = configService.get('PORT');
 
   await app.listen(port);
