334aec117b2ec5d90df91bc55e3c5f475ea317b7
Tuag <igortuag@gmail.com>
2023-09-18 10:13:32 -0300

    add port as variable

--- projects/third-project/src/main.ts
+++ projects/third-project/src/main.ts
@@ -6,7 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService);
+  const port = configService.get('PORT');
 
-  await app.listen(3333);
+  await app.listen(port);
 }
 bootstrap();
