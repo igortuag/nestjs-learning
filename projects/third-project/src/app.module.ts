46286ac6431f5b97c7056c86827287b4a835d010
Tuag <igortuag@gmail.com>
2023-09-19 08:27:41 -0300

    create auth module

--- projects/third-project/src/app.module.ts
+++ projects/third-project/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from 'auth/auth.modules';
 import { CreateAccountController } from 'controllers/create-account.controller';
 import { envSchema } from 'prisma/env';
 import { PrismaService } from 'prisma/prisma.service';
@@ -8,7 +9,9 @@ import { PrismaService } from 'prisma/prisma.service';
   imports: [ConfigModule.forRoot({
     validate: (envConfig) => envSchema.parse(envConfig),
     isGlobal: true,
-  })],
+  }),
+    AuthModule
+  ],
   controllers: [CreateAccountController],
   providers: [PrismaService],
 })
