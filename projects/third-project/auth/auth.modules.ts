fee9d64bd97655d7387d7ad8dff820099a77f891
Tuag <igortuag@gmail.com>
2023-09-21 08:30:53 -0300

    add private and public key

--- projects/third-project/auth/auth.modules.ts
+++ projects/third-project/auth/auth.modules.ts
@@ -10,10 +10,12 @@ import { Env } from 'prisma/env';
     JwtModule.registerAsync({
       inject: [ConfigService],
       useFactory(config: ConfigService<Env, true>) {
-        const secret = config.get('JWT_SECRET', { infer: true });
+        const privateKey = config.get('JWT_PRIVATE_KEY', { infer: true });
+        const publicKey = config.get('JWT_PUBLIC_KEY', { infer: true });
 
         return {
-          secret,
+          privateKey,
+          publicKey,
         }
       },
     })
