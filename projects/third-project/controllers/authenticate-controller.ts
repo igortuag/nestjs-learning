e14d0ff7d4b4f7a2ffb574cf47f4c3d4bdf80cd5
Tuag <igortuag@gmail.com>
2023-09-22 08:10:22 -0300

    add authenticateBodySchema

--- projects/third-project/controllers/authenticate-controller.ts
+++ projects/third-project/controllers/authenticate-controller.ts
@@ -1,5 +1,11 @@
 import { Controller, Post } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
+import { z } from "zod";
+
+const authenticateBodySchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+})
 
 @Controller('/sessions')
 export class AuthController {
