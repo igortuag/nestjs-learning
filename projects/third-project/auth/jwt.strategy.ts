be78700e4afde42c02dcff5d6850812ce18a96f7
Tuag <igortuag@gmail.com>
2023-09-24 09:45:19 -0300

    add validate schema

--- projects/third-project/auth/jwt.strategy.ts
+++ projects/third-project/auth/jwt.strategy.ts
@@ -2,7 +2,13 @@ import { ConfigService } from "@nestjs/config";
 import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from 'passport-jwt'
 import { Env } from "prisma/env";
+import { z } from "zod";
 
+const tokenSchema = z.object({
+  sub: z.string().uuid(),
+})
+
+type TokenSchema = z.infer<typeof tokenSchema>
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(config: ConfigService<Env, true>) {
     const publicKey = config.get('JWT_PUBLIC_KEY', { infer: true })
@@ -12,5 +18,9 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
       secretOrKey: publicKey,
       algorithms: ['RS256']
     })
+  }
+
+  async validate(payload: TokenSchema) {
+    return tokenSchema.parse(payload)
   }
 }
