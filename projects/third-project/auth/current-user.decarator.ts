e6b0f2e876be35f101c56bcd232c8ffebff29374
Tuag <igortuag@gmail.com>
2023-09-27 08:12:50 -0300

    add type TokenPayloadSchema

--- projects/third-project/auth/current-user.decarator.ts
+++ projects/third-project/auth/current-user.decarator.ts
@@ -1,9 +1,10 @@
 import { ExecutionContext, createParamDecorator } from '@nestjs/common'
+import { TokenPayloadSchema } from './jwt.strategy'
 
 export const CurrentUser = createParamDecorator(
   (_: never, context: ExecutionContext) => {
     const request = context.switchToHttp().getRequest()
 
-    return request.user
+    return request.user as TokenPayloadSchema
   }
 )
