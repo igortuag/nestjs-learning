353d2a7770d157784a302656d5e219baea412b27
Tuag <igortuag@gmail.com>
2023-09-27 08:11:49 -0300

    return user

--- projects/third-project/auth/current-user.decarator.ts
+++ projects/third-project/auth/current-user.decarator.ts
@@ -3,5 +3,7 @@ import { ExecutionContext, createParamDecorator } from '@nestjs/common'
 export const CurrentUser = createParamDecorator(
   (_: never, context: ExecutionContext) => {
     const request = context.switchToHttp().getRequest()
+
+    return request.user
   }
 )
