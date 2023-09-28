8574b387e83c63466e0839a7346d7f8bc0c32272
Tuag <igortuag@gmail.com>
2023-09-28 08:30:33 -0300

    add validation pipe

--- projects/third-project/controllers/create-question.controller.ts
+++ projects/third-project/controllers/create-question.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Post, Req, UseGuards } from "@nestjs/common";
+import { Body, Controller, Post, Req, UseGuards } from "@nestjs/common";
 import { CurrentUser } from "auth/current-user.decarator";
 import { JwtAuthGuard } from "auth/jwt-auth.guard";
 import { TokenPayloadSchema } from "auth/jwt.strategy";
 import { Request } from "express";
+import { ZodValidationPipe } from "pipes/zod-validation-pipes";
 import { PrismaService } from "prisma/prisma.service";
 import { z } from "zod";
 
@@ -19,7 +20,10 @@ export class CreateQuestionController {
   constructor(private prisma: PrismaService) { }
 
   @Post()
-  async handle(@CurrentUser() user: TokenPayloadSchema) {
+  async handle(
+    @CurrentUser() user: TokenPayloadSchema,
+    @Body(new ZodValidationPipe(createQuestionBodySchema)) body: CreateQuestionBody,
+  ) {
     console.log(user);
 
     return "ok";
