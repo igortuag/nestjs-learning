e79cd86e12d94480b77b9fb49f7ddf2486c7f344
Tuag <igortuag@gmail.com>
2023-10-01 07:44:07 -0300

    add page type

--- projects/third-project/controllers/fetch-recent-questions.controller.ts
+++ projects/third-project/controllers/fetch-recent-questions.controller.ts
@@ -1,6 +1,19 @@
 import { Controller, Get, Query, UseGuards } from "@nestjs/common";
 import { JwtAuthGuard } from "auth/jwt-auth.guard";
+import { ZodValidationPipe } from "pipes/zod-validation-pipes";
 import { PrismaService } from "prisma/prisma.service";
+import { z } from "zod";
+
+const pageQueryParamsSchema = z
+  .string()
+  .optional()
+  .default("1")
+  .transform(Number)
+  .pipe(z.number().min(1));
+
+const queryValidationPipe = new ZodValidationPipe(pageQueryParamsSchema);
+
+type PageQueryParamsSchema = z.infer<typeof pageQueryParamsSchema>;
 
 @Controller("/questions")
 @UseGuards(JwtAuthGuard)
@@ -8,7 +21,10 @@ export class FetchRecentQuestionsController {
   constructor(private prisma: PrismaService) {}
 
   @Get()
-  async handle(@Query("page") page: string) {
+  async handle(
+    @Query("page", queryValidationPipe)
+    page: PageQueryParamsSchema
+  ) {
     const questions = await this.prisma.question.findMany({
       orderBy: { createdAt: "desc" }
     });
