94121b052c10a500158bb0a33dc707e042a09409
Tuag <igortuag@gmail.com>
2023-10-01 07:40:03 -0300

    add page number on query

--- projects/third-project/controllers/fetch-recent-questions.controller.ts
+++ projects/third-project/controllers/fetch-recent-questions.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, UseGuards } from "@nestjs/common";
+import { Controller, Get, Query, UseGuards } from "@nestjs/common";
 import { JwtAuthGuard } from "auth/jwt-auth.guard";
 import { PrismaService } from "prisma/prisma.service";
 
@@ -8,7 +8,7 @@ export class FetchRecentQuestionsController {
   constructor(private prisma: PrismaService) {}
 
   @Get()
-  async handle() {
+  async handle(@Query("page") page: string) {
     const questions = await this.prisma.question.findMany({
       orderBy: { createdAt: "desc" }
     });
