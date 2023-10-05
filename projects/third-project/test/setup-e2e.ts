30bb6d5146278009fb38c1ed4e67bae6552be74e
Tuag <igortuag@gmail.com>
2023-10-05 09:02:41 -0300

    set schema id to url

--- projects/third-project/test/setup-e2e.ts
+++ projects/third-project/test/setup-e2e.ts
@@ -8,6 +8,8 @@ function generateUniqueDatabaseURL(schemaId: string) {
     throw new Error("DATABASE_URL is not set");
   }
   const url = new URL(process.env.DATABASE_URL);
+
+  url.searchParams.set("schema", schemaId);
 }
 
 beforeAll(async () => {
