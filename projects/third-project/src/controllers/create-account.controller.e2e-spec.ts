38bc30a509fa16f6f72c10e94c096ced3a990061
Tuag <igortuag@gmail.com>
2023-10-07 07:35:12 -0300

    test creation account

--- projects/third-project/src/controllers/create-account.controller.e2e-spec.ts
+++ projects/third-project/src/controllers/create-account.controller.e2e-spec.ts
@@ -1,6 +1,7 @@
 import { AppModule } from "@/app.module";
 import { INestApplication } from "@nestjs/common";
 import { Test } from "@nestjs/testing";
+import request from "supertest";
 
 test("Create account (E2E)", () => {
   let app: INestApplication;
@@ -14,5 +15,13 @@ test("Create account (E2E)", () => {
     await app.init();
   });
 
-  test("[POST] /accounts", async () => {});
+  test("[POST] /accounts", async () => {
+    const response = await request(app.getHttpServer()).post("/accounts").send({
+      name: "John Doe",
+      email: "johndoe@example.com",
+      password: "123456"
+    });
+
+    expect(response.status).toBe(201);
+  });
 });
