e80f345c05790981eb7dfb7921db1c54fbf5760d
Tuag <igortuag@gmail.com>
2023-10-07 07:32:44 -0300

    add structure to test create account

--- projects/third-project/src/controllers/create-account.controller.e2e-spec.ts
+++ projects/third-project/src/controllers/create-account.controller.e2e-spec.ts
@@ -1,3 +1,18 @@
-test("1 plus 1", () => {
-  expect(1 + 1).toBe(2);
+import { AppModule } from "@/app.module";
+import { INestApplication } from "@nestjs/common";
+import { Test } from "@nestjs/testing";
+
+test("Create account (E2E)", () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const module = await Test.createTestingModule({
+      imports: [AppModule]
+    }).compile();
+
+    app = module.createNestApplication();
+    await app.init();
+  });
+
+  test("[POST] /accounts", async () => {});
 });
